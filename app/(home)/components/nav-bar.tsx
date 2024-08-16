d9a3c627a222486c2964b202d5515ca23b7baeb7
haruBlank00 <harublank00@gmail.com>
2024-08-17 04:38:56 +0545

    install pwa and add dummy manifest

--- app/(home)/components/nav-bar.tsx
+++ app/(home)/components/nav-bar.tsx
@@ -1,8 +1,13 @@
+import LogoIcon from "@/components/icons/logo.icon"
 import Link from "next/link"
 
 export const NavBar = () => {
-  return <nav className="pborder-b-2 border-neutral-400 py-6">
-    <ul className="flex justify-end border-b-2 border-neutral-600 pb-2 px-6">
+  return <nav className="border-b-2 border-neutral-400 pt-6 pb-2 mb-8 flex items-center justify-between">
+    <figure>
+      <LogoIcon />
+      <figcaption className="sr-only">Blank Blog</figcaption>
+    </figure>
+    <ul className="flex justify-end">
       <li>
         <Link href={'/'} className="hover:underline underline-offset-2 transition text-lg">
           Home
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({});
 
 export default nextConfig;
